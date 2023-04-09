d13731cd553463f026be6ebf88e4461259c1f49b
fedorchenko-a <smolnax@gmail.com>
2023-04-09 15:14:46 +0300

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -11,8 +11,6 @@ terraform {
 # Configure AWS provider and creds
 provider "aws" {
   region                   = "us-east-1"
-  shared_config_files      = ["C:/Users/user/.aws/config"]
-  shared_credentials_files = ["C:/Users/user/.aws/credentials"]
   profile                  = "default"
 }
 
